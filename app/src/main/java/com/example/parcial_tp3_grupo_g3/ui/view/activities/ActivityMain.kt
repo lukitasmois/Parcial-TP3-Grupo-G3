f4c6947494d7fb92a76f1e3a1ce181fc684bdf27
lucasmoix <129341492+lucasmoix@users.noreply.github.com>
2024-05-28 21:53:43 -0300

    agregamos estructura general 2

--- app/src/main/java/com/example/parcial_tp3_grupo_g3/ui/view/activities/ActivityMain.kt
+++ app/src/main/java/com/example/parcial_tp3_grupo_g3/ui/view/activities/ActivityMain.kt
@@ -1,16 +1,17 @@
-package com.example.parcial_tp3_grupo_g3
+package com.example.parcial_tp3_grupo_g3.ui.view.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.parcial_tp3_grupo_g3.R
 
-class MainActivity : AppCompatActivity() {
+class ActivityMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.lay_activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/example/parcial_tp3_grupo_g3/ui/view/fragments/FragSearch.kt
@@ -0,0 +1,25 @@
+package com.example.parcial_tp3_grupo_g3.ui.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.parcial_tp3_grupo_g3.R
+
+class FragSearch : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.lay_frag_search, container, false)
+    }
+
+}
