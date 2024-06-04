f8c37f045b7e1143a31f3a2828c62499ab9e2dc8
milapesa <milapesamazzola@gmail.com>
2024-06-04 17:56:44 +0100

    Uploading the Splash Screen feature to the branch, including the layout and activity of the splash and a timer to move between activities.

---
+++ app/src/main/java/com/example/parcial_tp3_grupo_g3/ui/view/activities/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.example.parcial_tp3_grupo_g3.ui.view.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.parcial_tp3_grupo_g3.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashScreenActivity, ActivityMain::class.java)
+            startActivity(intent)
+        }, 3000)
+    }
+}
