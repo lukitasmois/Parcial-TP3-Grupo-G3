df39085d58daab436d4e780364e3db8ce8ab6825
lucasmoix <129341492+lucasmoix@users.noreply.github.com>
2024-06-04 20:07:30 -0300

    merge de main con profile-screen

--- app/src/main/java/com/example/parcial_tp3_grupo_g3/ui/view/activities/ActivityMain.kt
+++ app/src/main/java/com/example/parcial_tp3_grupo_g3/ui/view/activities/ActivityMain.kt
@@ -5,6 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.parcial_tp3_grupo_g3.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.example.parcial_tp3_grupo_g3.databinding.LayActivityMainBinding
