f11f7f13b61146723af0eb11d7c016221dbfefc6
lucasmoix <129341492+lucasmoix@users.noreply.github.com>
2024-06-06 00:53:57 -0300

    añadimos navegacion hacia atras

--- app/src/main/java/com/example/parcial_tp3_grupo_g3/ui/view/fragments/FragProfile.kt
+++ app/src/main/java/com/example/parcial_tp3_grupo_g3/ui/view/fragments/FragProfile.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.cardview.widget.CardView
 import androidx.navigation.fragment.findNavController
 import com.example.parcial_tp3_grupo_g3.R
@@ -13,6 +14,7 @@ import com.example.parcial_tp3_grupo_g3.R
 class FragProfile : Fragment() {
     lateinit var view1: View
     lateinit var cardSettings: CardView
+    lateinit var buttonBack : ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -24,8 +26,14 @@ class FragProfile : Fragment() {
         view1= inflater.inflate(R.layout.lay_frag_profile, container, false)
         view1.setBackgroundColor(resources.getColor(R.color.background_color))
         cardSettings = view1.findViewById(R.id.lay_profile_settingbutton)
+        buttonBack = view1.findViewById(R.id.frag_profile_backButton)
+
         cardSettings.setOnClickListener {
             findNavController().navigate(FragProfileDirections.actionFragProfileToFragSettings3())
+        }
+
+        buttonBack.setOnClickListener {
+            findNavController().navigate(FragProfileDirections.actionFragProfileToFragExplore())
         }
 
         return view1
